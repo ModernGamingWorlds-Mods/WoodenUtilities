678cc4c4d077a1e5a2153f17645b65a26cc516f7
lazynessmind <lazynessmind@gmail.com>
2020-10-17 13:09:49 +0100

    Fixed #3

--- src/main/java/com/lazy/woodenutilities/item/WoodenShearsItem.java
+++ src/main/java/com/lazy/woodenutilities/item/WoodenShearsItem.java
@@ -8,19 +8,15 @@ import net.minecraft.block.Blocks;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShearsItem;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ITag;
-import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
-import net.minecraftforge.common.ToolType;
 
-import java.util.List;
 import java.util.Random;
 
 public class WoodenShearsItem extends ShearsItem {
@@ -45,18 +41,20 @@ public class WoodenShearsItem extends ShearsItem {
 
     @Override
     public boolean onBlockDestroyed(ItemStack stackIn, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
-        if(Configs.DROP_SHEARS_PRODUCTS.get()){
+        if (Configs.DROP_SHEARS_PRODUCTS.get()) {
             Block block = state.getBlock();
-            if(isPresentOnTag(BlockTags.LEAVES, state) || block == Blocks.GRASS || block == Blocks.FERN || block == Blocks.DEAD_BUSH || block == Blocks.VINE){
+            if (isPresentOnTag(BlockTags.LEAVES, state) || block == Blocks.GRASS || block == Blocks.FERN || block == Blocks.DEAD_BUSH || block == Blocks.VINE) {
                 Block.spawnAsEntity(worldIn, pos, new ItemStack(state.getBlock().asItem()));
             }
         }
         return super.onBlockDestroyed(stackIn, worldIn, state, pos, entityLiving);
     }
 
-    public boolean isPresentOnTag(ITag.INamedTag<Block> tag, BlockState state){
-        for(Block block : tag.getAllElements()){
-            return state.isIn(block);
+    public boolean isPresentOnTag(ITag.INamedTag<Block> tag, BlockState state) {
+        for (Block block : tag.getAllElements()) {
+            if (state.isIn(block)) {
+                return true;
+            }
         }
         return false;
     }
@@ -69,5 +67,13 @@ public class WoodenShearsItem extends ShearsItem {
     @Override
     public float getDestroySpeed(ItemStack stack, BlockState state) {
         return super.getDestroySpeed(stack, state);
+    }
+
+    @Override
+    public ActionResultType itemInteractionForEntity(ItemStack stack, PlayerEntity playerIn, LivingEntity entity, Hand hand) {
+        if(Configs.DROP_SHEARS_PRODUCTS.get()){
+            return super.itemInteractionForEntity(stack, playerIn, entity, hand);
+        }
+        return ActionResultType.PASS;
     }
 }
