c614d3678299a2cb81dab4f4d958046a436ae217
lazynessmind <lazynessmind@gmail.com>
2020-10-31 22:30:34 +0000

    Only show tooltip if the charged slot is Empty

--- src/main/java/com/lazy/woodenutilities/client/screen/WoodenSolarPanelScreen.java
+++ src/main/java/com/lazy/woodenutilities/client/screen/WoodenSolarPanelScreen.java
@@ -5,10 +5,10 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Items;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TranslationTextComponent;
 
 import java.util.Collections;
 
@@ -25,15 +25,15 @@ public class WoodenSolarPanelScreen extends ContainerScreen<WoodenSolarPanelCont
         this.renderBackground(matrixStack);
         super.render(matrixStack, mouseX, mouseY, partialTicks);
         this.renderHoveredTooltip(matrixStack, mouseX, mouseY);
-        if (this.hoveredSlot == this.container.getSlot(0)) {
+        if (this.hoveredSlot == this.container.getSlot(0) && this.container.getSlot(0).getStack().getItem() == Items.AIR) {
             this.func_243308_b(matrixStack, Collections.singletonList(new StringTextComponent("You can charge items that hold energy in here.")), mouseX, mouseY);
         }
     }
 
     @Override
     protected void drawGuiContainerForegroundLayer(MatrixStack matrixStack, int mouseX, int mouseY) {
-        this.font.func_243248_b(matrixStack, this.title, (float)this.titleX, (float)this.titleY, 4210752);
-        this.font.func_243248_b(matrixStack, this.playerInventory.getDisplayName(), (float)this.playerInventoryTitleX, 39.0F, 4210752);
+        this.font.func_243248_b(matrixStack, this.title, (float) this.titleX, (float) this.titleY, 4210752);
+        this.font.func_243248_b(matrixStack, this.playerInventory.getDisplayName(), (float) this.playerInventoryTitleX, 39.0F, 4210752);
         this.font.func_243248_b(matrixStack, new StringTextComponent("Energy: " + this.container.getEnergy() + "/" + this.container.getMaxEnergy() + " (+" + this.container.getInput() + ")"), 8.0F, 23F, 4210752);
     }
 
