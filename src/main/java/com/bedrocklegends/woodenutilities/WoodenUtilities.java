21c5fc6d698548eba006412698e78328fcf6dd60
KyleBCox <kylecoxforweb@gmail.com>
2020-12-04 20:34:17 +0000

    added mod root class

--- src/main/java/com/bedrocklegends/woodenutilities/WoodenUtilities.java
+++ src/main/java/com/bedrocklegends/woodenutilities/WoodenUtilities.java
@@ -1,4 +1,8 @@
 package com.bedrocklegends.woodenutilities;
 
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(WoodenUtilities.ID)
 public class WoodenUtilities {
+    public static final String ID = "woodenutilities";
 }
