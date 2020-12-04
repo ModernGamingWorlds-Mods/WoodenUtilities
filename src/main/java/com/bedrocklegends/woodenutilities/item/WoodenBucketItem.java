f8790cf07166c8f8d0750914f30f960e8d3f000a
KyleBCox <kylecoxforweb@gmail.com>
2020-12-04 21:57:19 +0000

    added wooden bucket (without textures)

---
+++ src/main/java/com/bedrocklegends/woodenutilities/item/WoodenBucketItem.java
@@ -0,0 +1,176 @@
+package com.bedrocklegends.woodenutilities.item;
+
+import com.bedrocklegends.woodenutilities.setup.WoodenItems;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.IBucketPickupHandler;
+import net.minecraft.block.ILiquidContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.fluid.FlowingFluid;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BucketItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.stats.Stats;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.RayTraceContext;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.function.Supplier;
+
+public class WoodenBucketItem extends Item {
+    private final Fluid containedBlock;
+
+    public WoodenBucketItem(Supplier<? extends Fluid> supplier, Properties builder) {
+        super(builder);
+        this.fluidSupplier = supplier;
+        this.containedBlock = getFluid();
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        ItemStack itemstack = playerIn.getHeldItem(handIn);
+        BlockRayTraceResult raytraceresult = rayTrace(worldIn, playerIn, this.containedBlock == Fluids.EMPTY ? RayTraceContext.FluidMode.SOURCE_ONLY : RayTraceContext.FluidMode.NONE);
+        ActionResult<ItemStack> ret = net.minecraftforge.event.ForgeEventFactory.onBucketUse(playerIn, worldIn, itemstack, raytraceresult);
+        if (ret != null) return ret;
+        if (raytraceresult.getType() == RayTraceResult.Type.MISS) {
+            return ActionResult.resultPass(itemstack);
+        } else if (raytraceresult.getType() != RayTraceResult.Type.BLOCK) {
+            return ActionResult.resultPass(itemstack);
+        } else {
+            BlockRayTraceResult blockraytraceresult = raytraceresult;
+            BlockPos blockpos = blockraytraceresult.getPos();
+            Direction direction = blockraytraceresult.getFace();
+            BlockPos blockpos1 = blockpos.offset(direction);
+            if (worldIn.isBlockModifiable(playerIn, blockpos) && playerIn.canPlayerEdit(blockpos1, direction, itemstack)) {
+                if (this.containedBlock == Fluids.EMPTY) {
+                    BlockState blockstate1 = worldIn.getBlockState(blockpos);
+                    if (blockstate1.getBlock() instanceof IBucketPickupHandler) {
+                        Fluid fluid = ((IBucketPickupHandler)blockstate1.getBlock()).pickupFluid(worldIn, blockpos, blockstate1);
+                        if (fluid != Fluids.EMPTY) {
+                            playerIn.addStat(Stats.ITEM_USED.get(this));
+
+                            SoundEvent soundevent = this.containedBlock.getAttributes().getFillSound();
+                            if (soundevent == null) soundevent = fluid.isIn(FluidTags.LAVA) ? SoundEvents.ITEM_BUCKET_FILL_LAVA : SoundEvents.ITEM_BUCKET_FILL;
+                            playerIn.playSound(soundevent, 1.0F, 1.0F);
+                            ItemStack itemstack1 = DrinkHelper.fill(itemstack, playerIn, new ItemStack(WoodenItems.BUCKETS.get(fluid.getRegistryName().toString()).get()));
+                            if (fluid == Fluids.LAVA) {
+                                itemstack1 = ItemStack.EMPTY;
+                            }
+                            if (!worldIn.isRemote) {
+                                ItemStack stack = new ItemStack(WoodenItems.BUCKETS.get(fluid.getRegistryName().toString()).get());
+                                if (fluid == Fluids.LAVA) {
+                                    stack = ItemStack.EMPTY;
+                                }
+                                CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayerEntity)playerIn, stack);
+                            }
+                            return ActionResult.func_233538_a_(itemstack1, worldIn.isRemote());
+                        }
+                    }
+
+                    return ActionResult.resultFail(itemstack);
+                } else {
+                    BlockState blockstate = worldIn.getBlockState(blockpos);
+                    BlockPos blockpos2 = canBlockContainFluid(worldIn, blockpos, blockstate) ? blockpos : blockpos1;
+                    if (this.tryPlaceContainedLiquid(playerIn, worldIn, blockpos2, blockraytraceresult)) {
+                        this.onLiquidPlaced(worldIn, itemstack, blockpos2);
+                        if (playerIn instanceof ServerPlayerEntity) {
+                            CriteriaTriggers.PLACED_BLOCK.trigger((ServerPlayerEntity)playerIn, blockpos2, itemstack);
+                        }
+
+                        playerIn.addStat(Stats.ITEM_USED.get(this));
+                        return ActionResult.func_233538_a_(this.emptyBucket(itemstack, playerIn), worldIn.isRemote());
+                    } else {
+                        return ActionResult.resultFail(itemstack);
+                    }
+                }
+            } else {
+                return ActionResult.resultFail(itemstack);
+            }
+        }
+    }
+
+    private boolean canBlockContainFluid(World worldIn, BlockPos posIn, BlockState blockstate)
+    {
+        return blockstate.getBlock() instanceof ILiquidContainer && ((ILiquidContainer)blockstate.getBlock()).canContainFluid(worldIn, posIn, blockstate, this.containedBlock);
+    }
+
+
+    protected ItemStack emptyBucket(ItemStack stack, PlayerEntity player) {
+        return !player.abilities.isCreativeMode ? new ItemStack(WoodenItems.WOODEN_BUCKET.get()) : stack;
+    }
+
+    public void onLiquidPlaced(World worldIn, ItemStack p_203792_2_, BlockPos pos) {
+    }
+
+    public boolean tryPlaceContainedLiquid(@Nullable PlayerEntity player, World worldIn, BlockPos posIn, @Nullable BlockRayTraceResult rayTrace) {
+        if (!(this.containedBlock instanceof FlowingFluid)) {
+            return false;
+        } else {
+            BlockState blockstate = worldIn.getBlockState(posIn);
+            Block block = blockstate.getBlock();
+            Material material = blockstate.getMaterial();
+            boolean flag = blockstate.isReplaceable(this.containedBlock);
+            boolean flag1 = blockstate.isAir() || flag || block instanceof ILiquidContainer && ((ILiquidContainer)block).canContainFluid(worldIn, posIn, blockstate, this.containedBlock);
+            if (!flag1) {
+                return rayTrace != null && this.tryPlaceContainedLiquid(player, worldIn, rayTrace.getPos().offset(rayTrace.getFace()), (BlockRayTraceResult)null);
+            } else if (worldIn.getDimensionType().isUltrawarm() && this.containedBlock.isIn(FluidTags.WATER)) {
+                int i = posIn.getX();
+                int j = posIn.getY();
+                int k = posIn.getZ();
+                worldIn.playSound(player, posIn, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 0.5F, 2.6F + (worldIn.rand.nextFloat() - worldIn.rand.nextFloat()) * 0.8F);
+
+                for(int l = 0; l < 8; ++l) {
+                    worldIn.addParticle(ParticleTypes.LARGE_SMOKE, (double)i + Math.random(), (double)j + Math.random(), (double)k + Math.random(), 0.0D, 0.0D, 0.0D);
+                }
+
+                return true;
+            } else if (block instanceof ILiquidContainer && ((ILiquidContainer)block).canContainFluid(worldIn,posIn,blockstate,containedBlock)) {
+                ((ILiquidContainer)block).receiveFluid(worldIn, posIn, blockstate, ((FlowingFluid)this.containedBlock).getStillFluidState(false));
+                this.playEmptySound(player, worldIn, posIn);
+                return true;
+            } else {
+                if (!worldIn.isRemote && flag && !material.isLiquid()) {
+                    worldIn.destroyBlock(posIn, true);
+                }
+
+                if (!worldIn.setBlockState(posIn, this.containedBlock.getDefaultState().getBlockState(), 11) && !blockstate.getFluidState().isSource()) {
+                    return false;
+                } else {
+                    this.playEmptySound(player, worldIn, posIn);
+                    return true;
+                }
+            }
+        }
+    }
+
+    protected void playEmptySound(@Nullable PlayerEntity player, IWorld worldIn, BlockPos pos) {
+        SoundEvent soundevent = this.containedBlock.getAttributes().getEmptySound();
+        if(soundevent == null) soundevent = this.containedBlock.isIn(FluidTags.LAVA) ? SoundEvents.ITEM_BUCKET_EMPTY_LAVA : SoundEvents.ITEM_BUCKET_EMPTY;
+        worldIn.playSound(player, pos, soundevent, SoundCategory.BLOCKS, 1.0F, 1.0F);
+    }
+
+    @Override
+    public net.minecraftforge.common.capabilities.ICapabilityProvider initCapabilities(ItemStack stack, @Nullable net.minecraft.nbt.CompoundNBT nbt) {
+        if (this.getClass() == WoodenBucketItem.class)
+            return new net.minecraftforge.fluids.capability.wrappers.FluidBucketWrapper(stack);
+        else
+            return super.initCapabilities(stack, nbt);
+    }
+
+    private final java.util.function.Supplier<? extends Fluid> fluidSupplier;
+    public Fluid getFluid() { return fluidSupplier.get(); }
+
+}
--- src/main/java/com/bedrocklegends/woodenutilities/setup/WoodenItems.java
+++ src/main/java/com/bedrocklegends/woodenutilities/setup/WoodenItems.java
@@ -1,16 +1,38 @@
 package com.bedrocklegends.woodenutilities.setup;
 
 import com.bedrocklegends.woodenutilities.WoodenUtilities;
+import com.bedrocklegends.woodenutilities.item.WoodenBucketItem;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BucketItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class WoodenItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, WoodenUtilities.ID);
+    public static final Map<String, RegistryObject<Item>> BUCKETS = new HashMap<>();
+    public static final RegistryObject<Item> WOODEN_BUCKET = ITEMS.register("wooden_bucket", () -> new WoodenBucketItem(() -> Fluids.EMPTY, (new Item.Properties()).maxStackSize(16).group(ItemGroup.MISC)));
+
+    static {
+        for (Fluid fluid : ForgeRegistries.FLUIDS) {
+            if (fluid == Fluids.EMPTY || fluid.getRegistryName().getPath().startsWith("flowing")){
+                continue;
+            }
+            BUCKETS.put(Objects.requireNonNull(fluid.getRegistryName()).toString(), ITEMS.register("wooden_" + fluid.getRegistryName().getPath() + "_bucket", () -> new WoodenBucketItem(fluid::getFluid, (new Item.Properties()).maxStackSize(1).group(ItemGroup.MISC))));
+        }
+    }
 
     public static void register() {
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
+
 }
