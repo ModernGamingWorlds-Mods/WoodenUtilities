04a546c19dece46677fff2b4807f190c0fca4074
KyleBCox <kylecoxforweb@gmail.com>
2020-12-04 20:31:39 +0000

    removing old and adding new project for rewrite

---
+++ src/main/java/com/bedrocklegends/woodenutilities/WoodenUtilities.java
@@ -0,0 +1,4 @@
+package com.bedrocklegends.woodenutilities;
+
+public class WoodenUtilities {
+}
--- src/main/java/com/lazy/woodenutilities/impl/baubles/IRingWorn.java
+++
@@ -1,9 +0,0 @@
-package com.lazy.woodenutilities.impl.baubles;
-
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-
-public interface IRingWorn {
-
-    void onWorn(PlayerEntity playerEntity, ItemStack stack);
-}
