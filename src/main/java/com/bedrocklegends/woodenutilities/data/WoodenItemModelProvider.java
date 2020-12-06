a8b690e0b1312214745665e6761302cb97d43362
lazynessmind <lazynessmind@gmail.com>
2020-12-06 23:40:49 +0000

    Wooden Ring resource related things.

--- src/main/java/com/bedrocklegends/woodenutilities/data/WoodenItemModelProvider.java
+++ src/main/java/com/bedrocklegends/woodenutilities/data/WoodenItemModelProvider.java
@@ -25,5 +25,12 @@ public class WoodenItemModelProvider extends ItemModelProvider {
         }
         this.getBuilder(Objects.requireNonNull(WoodenItems.WOODEN_BUCKET.get().getRegistryName()).toString()).parent(new ModelFile.UncheckedModelFile("item/handheld")).texture("layer0", "items/wooden_bucket");
         this.getBuilder(Objects.requireNonNull(WoodenItems.WOODEN_PLATE.get().getRegistryName()).toString()).parent(new ModelFile.UncheckedModelFile("item/generated")).texture("layer0", "items/wooden_plate");
+        this.getBuilder(Objects.requireNonNull(WoodenItems.LUMBERJACK.get().getRegistryName()).toString()).parent(new ModelFile.UncheckedModelFile("item/generated")).texture("layer0", "items/wooden_ring");
+        this.getBuilder(Objects.requireNonNull(WoodenItems.NIGHT_VISION.get().getRegistryName()).toString()).parent(new ModelFile.UncheckedModelFile("item/generated")).texture("layer0", "items/wooden_ring");
+        this.getBuilder(Objects.requireNonNull(WoodenItems.HASTE.get().getRegistryName()).toString()).parent(new ModelFile.UncheckedModelFile("item/generated")).texture("layer0", "items/wooden_ring");
+        this.getBuilder(Objects.requireNonNull(WoodenItems.MIDAS_TOUCH.get().getRegistryName()).toString()).parent(new ModelFile.UncheckedModelFile("item/generated")).texture("layer0", "items/wooden_ring");
+        this.getBuilder(Objects.requireNonNull(WoodenItems.FLIGHT.get().getRegistryName()).toString()).parent(new ModelFile.UncheckedModelFile("item/generated")).texture("layer0", "items/wooden_ring");
+        this.getBuilder(Objects.requireNonNull(WoodenItems.MAGNET.get().getRegistryName()).toString()).parent(new ModelFile.UncheckedModelFile("item/generated")).texture("layer0", "items/wooden_ring");
+        this.getBuilder(Objects.requireNonNull(WoodenItems.REPAIR.get().getRegistryName()).toString()).parent(new ModelFile.UncheckedModelFile("item/generated")).texture("layer0", "items/wooden_ring");
     }
 }
