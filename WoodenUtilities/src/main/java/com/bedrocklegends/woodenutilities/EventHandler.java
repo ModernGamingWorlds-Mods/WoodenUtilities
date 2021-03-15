ff1fd652ff071907ba43b46b8330e11384039659
lazynessmind <lazynessmind@gmail.com>
2021-03-15 21:27:48 +0000

    Removed Mixin and Baubles Repo.

--- WoodenUtilities/src/main/java/com/bedrocklegends/woodenutilities/EventHandler.java
+++ WoodenUtilities/src/main/java/com/bedrocklegends/woodenutilities/EventHandler.java
@@ -1,21 +1,14 @@
 package com.bedrocklegends.woodenutilities;
 
-import com.bedrocklegends.woodenutilities.api.event.PostSaplingGrowEvent;
 import com.bedrocklegends.woodenutilities.api.resin.ResinProvider;
 import com.bedrocklegends.woodenutilities.api.resin.ResinProviderManager;
-import com.bedrocklegends.woodenutilities.setup.WoodenBlocks;
 import com.bedrocklegends.woodenutilities.setup.WoodenTags;
-import com.bedrocklegends.woodenutilities.tile.ResinExtractorTile;
 import net.minecraft.block.Block;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.TagsUpdatedEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import java.util.stream.Collectors;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class EventHandler {
@@ -30,22 +23,5 @@ public class EventHandler {
     @SubscribeEvent
     public static void onEntityUpdate(LivingEvent.LivingUpdateEvent event) {
         event.getEntityLiving().updateFluidHeightAndDoFluidPushing(WoodenTags.RESIN, 0.05D);
-    }
-
-    @SubscribeEvent
-    public static void onPostSaplingGrow(PostSaplingGrowEvent e) {
-        for (Direction value : Direction.Plane.HORIZONTAL.stream().collect(Collectors.toList())) {
-            BlockPos atDir = e.getPos().offset(value.getNormal());
-            Block blockAtDir = e.getWorld().getBlockState(atDir).getBlock();
-            if (blockAtDir == WoodenBlocks.RESIN_EXTRACTOR.get()) {
-                ResinExtractorTile resinExtractor = (ResinExtractorTile) e.getWorld().getBlockEntity(atDir);
-                if (resinExtractor != null) {
-                    if (resinExtractor.getFacingPos().equals(e.getPos())) {
-                        resinExtractor.startWorking();
-                        break;
-                    }
-                }
-            }
-        }
     }
 }
